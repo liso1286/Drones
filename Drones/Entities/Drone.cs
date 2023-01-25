0a5e36a66e688e20427932e73d2c9ba9b7826cae
Lisandro Hernandez <lhernandez@sifizsoft.com>
2023-01-25 02:37:28 -0500

    Agregar archivos de proyecto.

---
+++ Drones/Entities/Drone.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Drones.Entities
+{
+    public class Drone
+    {
+        [Key]
+        public int Id { get; set; }
+        [NotNull]
+        public string SerialNumber { get; set; }
+        [NotNull]
+        public string Model { get; set; }
+        [NotNull]
+        public decimal WeightLimit { get; set; }
+        [NotNull]
+        public decimal BatteryCapacity { get; set; }
+        [NotNull]
+        public string State { get; set; }
+        public IEnumerable<Medication> Medications { get; set; }
+
+        public Drone(
+            string serialNumber,
+            string model,
+            decimal weightLimit,
+            decimal batteryCapacity,
+            string state)
+        {
+            SerialNumber = serialNumber;
+            Model = model;
+            WeightLimit = weightLimit;
+            BatteryCapacity = batteryCapacity;
+            State = state;
+            Medications = new List<Medication>();
+        }
+    }
+}
---
+++ Drones/Entities/Medication.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Drones.Entities
+{
+    public class Medication
+    {
+        [Key]
+        public int Id { get; set; }
+        [NotNull]
+        public string Name { get; set; }
+        [NotNull]
+        public decimal Weight { get; set; }
+        [NotNull]
+        public string Code { get; set; }
+        [NotNull]
+        public byte[] Image { get; set; }
+
+        public Medication(string name, decimal weight, string code)
+        {
+            Name = name;
+            Weight = weight;
+            Code = code;
+            Image = Array.Empty<byte>();
+        }
+    }
+}
